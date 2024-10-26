09238d9fe6fc9cb7d4431453471d56ef99d63fb7
Illia Hrymalo <grimaloilya@gmail.com>
2024-10-25 21:02:46 -0600

    task completed

---
+++ src/js/1-timer.js
@@ -0,0 +1,90 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const dateTimePicker = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button');
+const daysElem = document.querySelector('[data-days]');
+const hoursElem = document.querySelector('[data-hours]');
+const minutesElem = document.querySelector('[data-minutes]');
+const secondsElem = document.querySelector('[data-seconds]');
+
+let userSelectedDate;
+startBtn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+
+    if (userSelectedDate <= new Date()) {
+      iziToast.error({
+        position: 'topRight',
+        color: '#EF4040',
+        message: 'Please choose a date in the future',
+        messageColor: '#fff',
+        iconUrl: '../img/icon-error.svg',
+        displayMode: 2,
+        timeout: 5000,
+        progressBar: false,
+      });
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+startBtn.addEventListener('click', handleClick);
+
+function handleClick() {
+  console.log(userSelectedDate);
+  startBtn.disabled = true;
+  dateTimePicker.disabled = true;
+
+  const intervalId = setInterval(() => {
+    const currentTime = Date.now();
+    let delta = userSelectedDate - currentTime;
+
+    if (delta <= 0) {
+      clearInterval(intervalId);
+      showTime({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      dateTimePicker.disabled = false;
+      return;
+    }
+
+    const time = convertMs(delta);
+    showTime(time);
+  }, 1000);
+}
+
+function showTime({ days, hours, minutes, seconds }) {
+  daysElem.textContent = addLeadingZero(days);
+  hoursElem.textContent = addLeadingZero(hours);
+  minutesElem.textContent = addLeadingZero(minutes);
+  secondsElem.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,55 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+const messageDefaultSettings = iziToast.settings({
+  titleColor: '#fff',
+  titleSize: 16,
+  messageColor: '#fff',
+  messageSize: 16,
+  progressBar: false,
+  position: 'topRight',
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const delay = form.delay.value;
+  const selectedValue = form.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (selectedValue === 'fulfilled') {
+        resolve(`Fulfilled promise in ${delay}ms`);
+      }
+      if (selectedValue === 'rejected') {
+        reject(`Rejected promise in ${delay}ms`);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(value => {
+      iziToast.show({
+        messageDefaultSettings,
+        title: 'OK',
+        message: `${value}`,
+        iconUrl: '../img/icon-ok.svg',
+        backgroundColor: 'rgb(89, 161, 13)',
+      });
+    })
+    .catch(value => {
+      iziToast.error({
+        messageDefaultSettings,
+        title: 'Error',
+        message: `${value}`,
+        iconUrl: '../img/icon-error.svg',
+        backgroundColor: 'rgb(239, 64, 64)',
+      });
+    });
+
+  form.reset();
+}
